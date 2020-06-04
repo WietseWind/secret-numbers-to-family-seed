931ab81930119f1a19de8eb2de04b7f797402bab
Wietse Wind <mail@wietse.com>
2020-06-04 02:23:50 +0200

    Beta

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app" class="container pt-3 pb-5">
+    <h3>XRPL Secret Numbers to Family Seed (s...)</h3>
+    <p>Generate a s... Family Seed (Secret) to use in other XRPL tools if you only have "Secret Numbers"</p>
+    <p class="alert alert-danger">
+      If you generated your Secret Numbers in a secure client like XUMM, converting your Secret Numbers to
+      a family seed to use in 3rd party applications MAY BE A RISK! If those 3rd party applications are
+      insecure, or you enter your Family Seed (secret) in an unsecure environment, or copy/paste on a
+      compromised device, you are at risk of LOSING ALL YOUR FUNDS.
+    </p>
+    <Derive />
+
+    <div class="mt-5 pt-3">
+      <div class="text-center">
+        By <a class="btn btn-primary btn-sm py-1" href="https://twitter.com/WietseWind" target="_blank"><b>@WietseWind</b></a>
+        <span class="d-print-none">
+          - <a class="btn btn-primary btn-sm py-1" href="https://github.com/WietseWind/secret-nubmers-to-family-seed/releases" target="_blank"><b>Download (Github)</b></a>
+          - <a class="btn btn-outline-primary btn-sm py-1" href="https://github.com/WietseWind/secret-nubmers-to-family-seed" target="_blank"><b>Source code (Github)</b></a>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Derive from './components/Derive.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Derive
+  },
+  data () {
+    return {
+    }
+  },
+  watch: {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+  @media print {
+    .d-print-none {
+      display: none!important;
+    }
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  html, body {
+    -webkit-touch-callout: none; /* iOS Safari */
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+          -ms-user-select: none; /* Internet Explorer/Edge */
+              user-select: none; /* Non-prefixed version, currently
+                                    supported by Chrome, Edge, Opera and Firefox */
+  }
+</style>
---
+++ src/components/Derive.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="hello pt-2">
+    <div class="card shadow mb-3 d-print-none" v-show="familySeed.slice(0, 1) === 'X'">
+      <div class="card-header"><b>Please enter your secret numbers</b></div>
+      <div class="card-body">
+        <div class="row pb-1" v-for="i in Object.keys(secret)" v-bind:key="i">
+          <div class="col-3 col-md-4 text-right"><b class="text-primary pt-1 d-block">{{ 'ABCDEFGH'.slice(i).slice(0, 1) }}</b></div>
+          <div class="col-8 col-md-5"><input :class="{ 'is-valid': checkChecksum(i), 'is-invalid': checkChecksum(i) === false }" maxlength="6" v-model="secret[i]" type="text" class="form-control form-control-sm text-secondary" placeholder="XXXXXX (Please enter)" /></div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center" v-show="familySeed.slice(0, 1) !== 'X'">
+      <b>Account address (public)</b>
+      <br />
+      <code class="generated font-weight-bold px-2 text-primary d-inline-block mb-2" v-clipboard:copy="'Account: ' + account">{{ account }}</code>
+      <br />
+      <b>Account secret (for your eyes only)</b>
+      <br />
+      <small>
+        <b>Family Seed</b> (Alternative but common secret format)
+        <br />
+        <code class="generated px-2 text-danger d-inline-block mb-2" v-clipboard:copy="'Family Seed Secret (keep safe!): ' + familySeed">
+          {{ familySeed }}
+        </code>
+      </small>
+    </div>
+    <div class="mt-4 text-center d-print-none" v-show="familySeed.slice(0, 1) !== 'X'">
+      <button @click="print()" class="font-weight-bold btn btn btn-dark">Print this (unsafe!) <small>- Better write down &amp; test</small></button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
+// import XrplKeyPairs from 'ripple-keypairs'
+import { Account, Utils } from 'xrpl-secret-numbers'
+
+Vue.use(VueClipboard)
+
+export default {
+  name: 'Derive',
+  methods: {
+    print () {
+      window.print()
+    },
+    checkChecksum (i) {
+      try {
+        return Utils.checkChecksum(i, this.secret[i])
+      } catch (e) {}
+    },
+    calc () {
+      const account = new Account(this.secret)
+      this.account = account.account.address
+      this.familySeed = account.account.familySeed
+    }
+  },
+  watch: {
+    secret () {
+      if (Object.keys(this.secret).filter(i => { return this.checkChecksum(i) }).length === 8) {
+        this.calc()
+      } else {
+        this.account = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        this.familySeed = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+      }
+    }
+  },
+  data () {
+    return {
+      account: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      secret: ['', '', '', '', '', '', '', ''],
+      familySeed: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  input[type='text'] {
+    font-weight: bold;
+  }
+  code {
+    cursor: pointer;
+    letter-spacing: .1em;
+    font-size: 1.5em;
+    &:active {
+      color: white !important;
+      background-color: rgba(0, 0, 0, .8);
+      outline: 0;
+      box-shadow: 0 0 0 3px rgba(0, 123, 255, .5);
+      border-radius: 4px;
+      z-index: 10;
+      position: relative;
+      &:before {
+        content: 'Copied! (Unsafe!)';
+        background-color: rgba(200, 0, 0, .9);
+        padding: 1px 4px;
+        border-radius: 4px;
+        display: block;
+        position: absolute;
+        color: white;
+        font-weight: bold;
+        border: 1px solid rgba(0, 0, 0, .5);
+        font-size: 12px;
+        margin-top: -28px;
+        margin-left: -10px;
+        letter-spacing: normal;
+      }
+    }
+  }
+</style>
